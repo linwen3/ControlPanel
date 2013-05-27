79e0469d77b962c8114b1d3814bb41a0b7581f74
aloo <aloocs@gmail.com>
2013-05-27 21:40:07 +0800

    1, complete the simulate data

--- server/data.js
+++ server/data.js
@@ -7,31 +7,68 @@
  */
 
 // simulate the data for manipulating
-var data = {[
-    {
+var data = {
+    results: [
+        {
         "id": "1",
-        "bill": "SuperWidget",
-        "colour": "Green",
-        "price": "19.99"
+        "status": "New",
+        "date": "01/06/2013",
+        "time": "24:00",
+        "Multi/Single": "Both",
+        "SIOC": "Non-SIOC",
+        "Gilt": "yes",
+        "Hazmat": "yes",
+        "ASIN": "12341234"
+    },{
+        "id": "2",
+        "status": "New",
+        "date": "01/06/2013",
+        "time": "24:00",
+        "Multi/Single": "Single",
+        "SIOC": "Non-SIOC",
+        "Gilt": "yes",
+        "Hazmat": "yes",
+        "ASIN": "1234123455123"
     },{
         "id": "3",
-        "bill": "SuperWidget",
-        "colour": "Green",
-        "price": "19.99"
+        "status": "New",
+        "date": "01/06/2013",
+        "time": "24:00",
+        "Multi/Single": "Multi",
+        "SIOC": "SIOC",
+        "Gilt": "yes",
+        "Hazmat": "no",
+        "ASIN": "123451234"
     },{
         "id": "4",
-        "bill": "SuperWidget",
-        "colour": "Green",
-        "price": "19.99"
+        "status": "Canceled",
+        "date": "01/06/2013",
+        "time": "24:00",
+        "Multi/Single": "Single",
+        "SIOC": "Both",
+        "Gilt": "no",
+        "Hazmat": "yes",
+        "ASIN": "1241234"
     },{
         "id": "5",
-        "bill": "SuperWidget",
-        "colour": "Green",
-        "price": "19.99"
+        "status": "Packed",
+        "date": "01/06/2013",
+        "time": "24:00",
+        "Multi/Single": "Single",
+        "SIOC": "SIOC",
+        "Gilt": "no",
+        "Hazmat": "no",
+        "ASIN": "123412431234"
     },{
         "id": "6",
-        "bill": "SuperWidget",
-        "colour": "Green",
-        "price": "19.99"
+        "status": "Accepted",
+        "date": "01/06/2013",
+        "time": "24:00",
+        "Multi/Single": "Both",
+        "SIOC": "SIOC",
+        "Gilt": "yes",
+        "Hazmat": "yes",
+        "ASIN": "1234151234"
     }
-    ]};
+    ]
+}
