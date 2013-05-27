426627b9daffeb85337f3ae95a3b7680ab39eeaf
aloo <aloocs@Gmail.com>
2013-05-27 18:50:18 +0800

    1，加入了list和filter

---
+++ server/data.js
@@ -0,0 +1,37 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Administrator
+ * Date: 13-5-27
+ * Time: 下午4:44
+ * To change this template use File | Settings | File Templates.
+ */
+
+// simulate the data for manipulating
+var data = {[
+    {
+        "id": "1",
+        "bill": "SuperWidget",
+        "colour": "Green",
+        "price": "19.99"
+    },{
+        "id": "3",
+        "bill": "SuperWidget",
+        "colour": "Green",
+        "price": "19.99"
+    },{
+        "id": "4",
+        "bill": "SuperWidget",
+        "colour": "Green",
+        "price": "19.99"
+    },{
+        "id": "5",
+        "bill": "SuperWidget",
+        "colour": "Green",
+        "price": "19.99"
+    },{
+        "id": "6",
+        "bill": "SuperWidget",
+        "colour": "Green",
+        "price": "19.99"
+    }
+    ]};
